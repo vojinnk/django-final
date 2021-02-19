ef078dd2106781721d8a285fb420f1a2f512ba18
Mirko998 <nk.mirko998@gmail.com>
2021-02-19 16:37:53 +0100

    create branch product (for me and Vojin)

--- music_shop/music_shop/urls.py
+++ music_shop/music_shop/urls.py
@@ -1,9 +1,9 @@
 
 urlpatterns = [
-    url(r'^user/', include('user.urls')),
-    url(r'^product/', include('products.urls')),
-    url(r'^orders/', include('orders.urls')),
-    url(r'^seller/', include('seller.urls')),
-    url(r'^cart/', include('cart.urls')),
+  #  url(r'^user/', include('user.urls')),
+   # url(r'^product/', include('products.urls')),
+   # url(r'^orders/', include('orders.urls')),
+   # url(r'^seller/', include('seller.urls')),
+   # url(r'^cart/', include('cart.urls')),
     #path('admin/', admin.site.urls),
 ]
